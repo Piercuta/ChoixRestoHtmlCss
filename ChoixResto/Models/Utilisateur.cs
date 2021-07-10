745ad06cf6cdbe800ac249fcf6e712876df083b3
Pierre Courteille <pcourteille@premaccess.com>
2021-07-10 17:59:52 +0200

    add project

---
+++ ChoixResto/Models/Utilisateur.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChoixResto.Models
+{
+	public class Utilisateur
+	{
+		public int Id { get; set; }
+		public string Prenom { get; set; }
+		public string Password { get; set; }
+		public string Role { get; set; }
+	}
+}
