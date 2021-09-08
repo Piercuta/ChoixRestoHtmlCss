7715c8c376bcb57cee6af9496fa5daeda8ef076f
Pierre Courteille <pcourteille@premaccess.com>
2021-09-08 12:07:15 +0200

    Add display prénom utilisateur

--- ChoixResto/Models/Utilisateur.cs
+++ ChoixResto/Models/Utilisateur.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace ChoixResto.Models
 	public class Utilisateur
 	{
 		public int Id { get; set; }
+		[Display(Name = "Prénom")]
 		public string Prenom { get; set; }
 		public string Password { get; set; }
 		public string Role { get; set; }
