b80e3f860691bfe31780fee013149fe0eb0e2595
Pierre Courteille <pcourteille@premaccess.com>
2021-11-04 19:57:53 +0100

    git push -f origin/masterremove rm from stop app

--- .aws/startApplication.sh
+++ .aws/startApplication.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-cd /etc/profile.d/
-./load_env.sh
-cp -R /var/www/my-temp-dir /var/www/${ApplicationName}
+ApplicationName=$(aws ssm get-parameters --region eu-west-1 --names ApplicationName --with-decryption --query Parameters[0].Value)
+echo "${ApplicationName}"
+# we remove first and last quote get with ssm
+sudo cp -R /var/www/my-temp-dir/* /var/www/${ApplicationName:1:-1}
 echo "start application"
-cd /var/www/${ApplicationName}/
+cd /var/www/${ApplicationName:1:-1}/
 echo "dotnet ChoixResto.dll > /dev/null 2>&1 &"
 dotnet ChoixResto.dll > /dev/null 2>&1 &
-
+cd  /var/www/my-temp-dir/
+rm -rf *
 
--- .aws/stopApplication.sh
+++ .aws/stopApplication.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-cd /etc/profile.d/
-./load_env.sh
+ApplicationName=$(aws ssm get-parameters --region eu-west-1 --names ApplicationName --with-decryption --query Parameters[0].Value)
 echo "stop application"
-cd  /var/www/${ApplicationName}/
+cd  /var/www/${ApplicationName:1:-1}/
 echo "kill $(ps aux | grep '[C]hoixResto.dll' | awk '{print $2}')"
 kill $(ps aux | grep '[C]hoixResto.dll' | awk '{print $2}') || echo "Process ChoixResto.dll was not running."
-cd  /var/www/my-temp-dir/
-rm -rf *
