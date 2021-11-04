b07cbbb6210494d450c525d6c50edd7809a4c058
Pierre Courteille <pcourteille@premaccess.com>
2021-11-04 17:46:17 +0100

    add my-application folder instead of choixresto and ApplicationName param in cf

--- .aws/startApplication.sh
+++ .aws/startApplication.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "start application"
-cd /var/www/choixresto/site/
+cd /var/www/my-application/site/
 echo "dotnet ChoixResto.dll > /dev/null 2>&1 &"
 dotnet ChoixResto.dll > /dev/null 2>&1 &
 
