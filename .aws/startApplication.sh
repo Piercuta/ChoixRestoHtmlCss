807cef159b6f2f1e74e642e2c82e367d5b8df169
Pierre Courteille <pcourteille@premaccess.com>
2021-11-04 21:50:00 +0100

    last commit 2 paramstore

--- .aws/startApplication.sh
+++ .aws/startApplication.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 ApplicationName=$(aws ssm get-parameters --region eu-west-1 --names ApplicationName --with-decryption --query Parameters[0].Value)
+DllToStart=$(aws ssm get-parameters --region eu-west-1 --names DllToStart --with-decryption --query Parameters[0].Value)
 echo "${ApplicationName}"
 # we remove first and last quote get with ssm
 sudo cp -R /var/www/my-temp-dir/* /var/www/${ApplicationName:1:-1}
 echo "start application"
 cd /var/www/${ApplicationName:1:-1}/
-echo "dotnet ChoixResto.dll > /dev/null 2>&1 &"
-dotnet ChoixResto.dll > /dev/null 2>&1 &
+echo "dotnet ${DllToStart:1:-1} > /dev/null 2>&1 &"
+dotnet ${DllToStart:1:-1} > /dev/null 2>&1 &
 cd  /var/www/my-temp-dir/
 rm -rf *
 
--- .aws/stopApplication.sh
+++ .aws/stopApplication.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ApplicationName=$(aws ssm get-parameters --region eu-west-1 --names ApplicationName --with-decryption --query Parameters[0].Value)
+DllToStart=$(aws ssm get-parameters --region eu-west-1 --names DllToStart --with-decryption --query Parameters[0].Value)
 echo "stop application"
 cd  /var/www/${ApplicationName:1:-1}/
-echo "kill $(ps aux | grep '[C]hoixResto.dll' | awk '{print $2}')"
-kill $(ps aux | grep '[C]hoixResto.dll' | awk '{print $2}') || echo "Process ChoixResto.dll was not running."
+echo "kill $(ps aux | grep '${DllToStart:1:-1}' | awk '{print $2}')"
+kill $(ps aux | grep '${DllToStart:1:-1}' | awk '{print $2}') || echo "Process ChoixResto.dll was not running."
