e755f448002f7e0802534183dede25efe34f776f
Pierre Courteille <pcourteille@premaccess.com>
2021-11-08 16:13:50 +0100

    remove new and moddify stop application

--- .aws/stopApplication.sh
+++ .aws/stopApplication.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ApplicationName=$(aws ssm get-parameters --region eu-west-1 --names ApplicationName --with-decryption --query Parameters[0].Value)
 DllToStart=$(aws ssm get-parameters --region eu-west-1 --names DllToStart --with-decryption --query Parameters[0].Value)
+Dll=${DllToStart:1:-1} 
 echo "stop application"
 cd  /var/www/${ApplicationName:1:-1}/
-echo "kill $(ps aux | grep '${DllToStart:1:-1}' | awk '{print $2}')"
-kill $(ps aux | grep '${DllToStart:1:-1}' | awk '{print $2}') || echo "Process ChoixResto.dll was not running."
+echo "kill $(ps aux | grep $Dll | awk '{print $2}')"
+kill $(ps aux | grep $Dll | awk '{print $2}') || echo "Process ChoixResto.dll was not running."
