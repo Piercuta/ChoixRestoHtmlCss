a5a140983109d1094deb977ae6288492e9d9dbd5
Piercuta <pierre.courteille@gmail.com>
2021-12-15 11:28:20 +0100

    rm ssm param from bash script

--- .aws/startApplication.sh
+++ .aws/startApplication.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+
 # to remove once you set bash_profile correctly
 export DOTNET_ROOT=/tmp/dotnet
 export PATH=$PATH:$DOTNET_ROOT
 # end to remove
+
 cd /var/www/
 Env=$(cat env.txt)
 ApplicationName=$(cat application_name.txt)
 DllToStart=$(cat dll_to_start.txt)
-# ApplicationName=$(aws ssm get-parameters --region eu-west-1 --names $Env-$AppName --with-decryption --query Parameters[0].Value)
-# DllToStart=$(aws ssm get-parameters --region eu-west-1 --names $Env-$AppName-dll-to-start --with-decryption --query Parameters[0].Value)
 echo ${Env}
 echo ${ApplicationName}
 echo ${DllToStart} 
--- .aws/stopApplication.sh
+++ .aws/stopApplication.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 cd /var/www/
 Env=$(cat env.txt)
-AppName=$(cat application_name.txt)
-ApplicationName=$(aws ssm get-parameters --region eu-west-1 --names $Env-$AppName --with-decryption --query Parameters[0].Value)
-DllToStart=$(aws ssm get-parameters --region eu-west-1 --names $Env-$AppName-dll-to-start --with-decryption --query Parameters[0].Value)
-Dll=${DllToStart:1:-1} 
+ApplicationName=$(cat application_name.txt)
+DllToStart=$(cat dll_to_start.txt)
 echo "stop application"
 cd  /var/www/${ApplicationName:1:-1}/
-echo "kill $(ps aux | grep $Dll | awk '{print $2}')"
-kill $(ps aux | grep $Dll | awk '{print $2}') || echo "Process $Dll was not running."
+echo "kill $(ps aux | grep $DllToStart | awk '{print $2}')"
+kill $(ps aux | grep $DllToStart | awk '{print $2}') || echo "Process $DllToStart was not running."
