b24897a6dbd278b15a6b1d68ea912f8373ca1214
Piercuta <pierre.courteille@gmail.com>
2021-12-15 10:54:37 +0100

    rm ssm

--- .aws/startApplication.sh
+++ .aws/startApplication.sh
@@ -5,16 +5,18 @@ export PATH=$PATH:$DOTNET_ROOT
 # end to remove
 cd /var/www/
 Env=$(cat env.txt)
-AppName=$(cat application_name.txt)
-ApplicationName=$(aws ssm get-parameters --region eu-west-1 --names $Env-$AppName --with-decryption --query Parameters[0].Value)
-DllToStart=$(aws ssm get-parameters --region eu-west-1 --names $Env-$AppName-dll-to-start --with-decryption --query Parameters[0].Value)
+ApplicationName=$(cat application_name.txt)
+DllToStart=$(cat dll_to_start.txt)
+# ApplicationName=$(aws ssm get-parameters --region eu-west-1 --names $Env-$AppName --with-decryption --query Parameters[0].Value)
+# DllToStart=$(aws ssm get-parameters --region eu-west-1 --names $Env-$AppName-dll-to-start --with-decryption --query Parameters[0].Value)
+echo ${Env}
 echo ${ApplicationName}
 echo ${DllToStart} 
 # we remove first and last quote get with ssm
-sudo cp -R /var/www/my-temp-dir/* /var/www/${ApplicationName:1:-1}
+sudo cp -R /var/www/my-temp-dir/* /var/www/${ApplicationName}
 echo "start application"
-cd /var/www/${ApplicationName:1:-1}/
-echo "dotnet ${DllToStart:1:-1} > /dev/null 2>&1 &"
-dotnet ${DllToStart:1:-1} > /dev/null 2>&1 &
+cd /var/www/${ApplicationName}/
+echo "dotnet ${DllToStart} > /dev/null 2>&1 &"
+dotnet ${DllToStart} > /dev/null 2>&1 &
 cd  /var/www/my-temp-dir/
 rm -rf *
