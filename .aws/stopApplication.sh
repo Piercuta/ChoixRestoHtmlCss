2bb3ef06db7b53dac8b1fbb43625dc79debbc96d
Piercuta <pierre.courteille@gmail.com>
2022-01-05 18:45:52 +0100

    update sh files

--- .aws/stopApplication.sh
+++ .aws/stopApplication.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "stop application"
-cd  /var/www/my-temp-dir/
+cd  /var/www/website/
 echo "kill $(ps aux | grep ChoixResto.dll | awk '{print $2}')"
 kill $(ps aux | grep ChoixResto.dll | awk '{print $2}') || echo "Process ChoixResto.dll was not running."
-rm -rf /var/www/my-temp-dir/
+rm -rf /var/www/website/
